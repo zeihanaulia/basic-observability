afa277174226f12bcc4899696aba20e56b4f0eac
Zeihan Aulia <zeihan@warungpintar.co>
2021-03-24 16:51:31 +0700

    add middleware logger

---
+++ pkg/middleware/logger.go
@@ -0,0 +1,39 @@
+pakcage middleware
+
+import (
+	"time"
+	"net/http"
+	mylog "github.com/zeihanaulia/go-async-request/pkg/log"
+)
+
+type LoggerConfig struct {
+	SkipURLPath []string
+}
+
+func Logger(log mylog.Factory, cfg LoggerConfig) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+			if len(cfg.SkipURLPath) > 0 {
+				if contains(cfg.SkipURLPath, r.URL.Path) {
+					next.ServeHTTP(w, r)
+					return
+				}
+			}
+
+			log.Bg().Info(
+				"Start Request", 
+				zap.Any("start_at", time.Now().Format(time.RFC3339)),
+				zap.String("method", r.Method),
+				zap.String("url", r.URL.Path),
+			)
+
+			defer log.Bg().Info(
+				"End Request", 
+				zap.Any("start_at", time.Now().Format(time.RFC3339)),
+				zap.String("method", r.Method),
+				zap.String("url", r.URL.Path),
+			)
+		})
+	}
+}
